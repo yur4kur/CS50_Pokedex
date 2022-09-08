aa0476e6211f2447ba422bec0db833ffbcbb4590
Iurii Kurinnoi <yur4kur@icloud.com>
2022-09-08 14:50:25 +0400

    Initial Commit

---
+++ CS50_Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS50_Pokedex
+//
+//  Created by Юрий Куринной on 08.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
